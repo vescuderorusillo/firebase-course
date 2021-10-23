483cf357871bc4018244d5fe541ccf2f57e102a0
Victor Escudero <victor.escudero@bestsecret.com>
2021-10-23 19:20:48 +0200

    Created some queries to Firestore db

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -40,7 +40,79 @@ export class AboutComponent {
         return newData;
     }
 
+    onReadDoc() {
+        this.db.doc("/courses/0ItcoZauN4gjTWmSMySm").get()
+            .subscribe(snap => {
+                console.log(snap.id);
+                console.log(snap.data());
+            });
+    }
 
+    onReadCollection() {
+        this.db.collection("courses").get()
+            .subscribe(snaps => {
+                snaps.forEach(snap => {
+                    console.log(snap.id);
+                    console.log(snap.data());
+                });
+            });
+    }
+
+    onReadCollectionFiltered() {
+        this.db.collection("/courses/0ItcoZauN4gjTWmSMySm/lessons",
+            ref=> ref.where("seqNo", "<=", 5).orderBy("seqNo")
+        ).get()
+            .subscribe(snaps => {
+                snaps.forEach(snap => {
+                    console.log(snap.id);
+                    console.log(snap.data());
+                });
+            });
+    }
+
+    onReadQueryWithComposedIndex() {
+        this.db.collection("/courses",
+            ref=> ref.where("seqNo", "<=", 20)
+                .where("url", "==", "angular-forms-course")
+                .orderBy("seqNo")
+        ).get()
+            .subscribe(snaps => {
+                snaps.forEach(snap => {
+                    console.log(snap.id);
+                    console.log(snap.data());
+                });
+            });
+    }
+
+    onReadCollectionGroup() {
+        this.db.collectionGroup("lessons", ref => 
+            ref.where("seqNo", "==", 1))
+            .get()
+            .subscribe(snaps => {
+                snaps.forEach(snap => {
+                    console.log(snap.id);
+                    console.log(snap.data());
+                });
+            });
+    }
+
+    //RealTime Capabilities
+    onReadDocInRealTime() {
+        this.db.doc("/courses/0ItcoZauN4gjTWmSMySm")
+            .snapshotChanges()
+            .subscribe(snap => {
+                console.log(snap.payload.id);
+                console.log(snap.payload.data());
+            });
+    }
+
+    onReadValueInRealTime() {
+        this.db.doc("/courses/0ItcoZauN4gjTWmSMySm")
+            .valueChanges()
+            .subscribe(course => {
+                console.log(course);
+            });
+    }
 }
 
 
