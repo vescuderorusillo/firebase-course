54aa1a989ac5f2b82f762186036766689f91f69e
Victor Escudero <victor.escudero@bestsecret.com>
2021-10-27 20:48:18 +0200

    Logic to pass course data to a Dialog and to close it

--- src/app/edit-course-dialog/edit-course-dialog.component.ts
+++ src/app/edit-course-dialog/edit-course-dialog.component.ts
@@ -1,7 +1,7 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
 import {Course} from "../model/course";
-import {FormBuilder, Validators, FormGroup} from "@angular/forms";
+import {FormBuilder, Validators, FormGroup, Form} from "@angular/forms";
 import {AngularFireStorage} from '@angular/fire/storage';
 import {Observable} from 'rxjs';
 
@@ -13,8 +13,27 @@ import {Observable} from 'rxjs';
 })
 export class EditCourseDialogComponent {
 
-    constructor() {
+    form: FormGroup;
 
+    course: Course;
+
+    constructor(
+        private dialogRef: MatDialogRef<EditCourseDialogComponent>,
+        private fb: FormBuilder,
+        @Inject(MAT_DIALOG_DATA) course: Course,
+    ) {
+        this.course = course;
+
+        this.form = this.fb.group({
+            description: [course.description, Validators.required],
+            longDescription: [course.longDescription, Validators.required],
+            promo: [course.promo]
+        });
+
+    }
+
+    close() {
+        this.dialogRef.close();
     }
 }
 
