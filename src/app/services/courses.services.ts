8da76769a03aa964cfc7cacc0ef916ab95080ba1
Victor Escudero <victor.escudero@bestsecret.com>
2021-10-30 22:07:22 +0200

    Created method to find lessons of a course

--- src/app/services/courses.services.ts
+++ src/app/services/courses.services.ts
@@ -6,6 +6,9 @@ import { Course } from "../model/course";
 import { Lesson } from "../model/lesson";
 import { convertSnaps } from "./db-utils";
 
+import firebase from "firebase";
+import OrderByDirection = firebase.firestore.OrderByDirection;
+
 @Injectable({
     providedIn: "root"
 })
@@ -13,6 +16,20 @@ export class CoursesService {
     
     constructor(private db: AngularFirestore){
 
+    }
+
+    findLessons(courseId: string, sortOrder: OrderByDirection = 'asc', 
+                    pageNumber = 0, pageSize = 3) : Observable<Lesson[]> {
+        
+        return this.db.collection(`courses/${courseId}/lessons`, 
+            ref => ref.orderBy("seqNo", sortOrder)
+                .limit(pageSize)
+                .startAfter(pageNumber * pageSize)
+        )
+        .get()
+        .pipe(
+            map(results => convertSnaps<Lesson>(results))
+        );
     }
 
     findCourseByUrl(courseUrl: string) : Observable<Course | null> {
