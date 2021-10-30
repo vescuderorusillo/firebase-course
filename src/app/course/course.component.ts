4e8def01fe5db4821acf2a05242b2625e839b6f1
Victor Escudero <victor.escudero@bestsecret.com>
2021-10-30 22:25:48 +0200

    Logic to load and paginate lessons

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -4,6 +4,7 @@ import {Course} from '../model/course';
 import {finalize, tap} from 'rxjs/operators';
 import {Observable} from 'rxjs';
 import {Lesson} from '../model/lesson';
+import { CoursesService } from '../services/courses.services';
 
 @Component({
   selector: 'course',
@@ -14,16 +15,45 @@ export class CourseComponent implements OnInit {
 
   course: Course;
 
+  lessons: Lesson[];
+
   loading = false;
+
+  lastPageLoaded = 0;
 
   displayedColumns = ['seqNo', 'description', 'duration'];
 
-  constructor(private route: ActivatedRoute) {
+  constructor(
+    private route: ActivatedRoute,
+    private coursesService: CoursesService) {
 
   }
 
   ngOnInit() {
     this.course = this.route.snapshot.data["course"];
+
+    this.loading = true;
+
+    this.coursesService.findLessons(this.course.id)
+      .pipe(
+        finalize(() => this.loading = false)
+      )
+      .subscribe(
+        lessons => this.lessons = lessons
+      );
+  }
+
+  loadMore() {
+    this.lastPageLoaded++;
+    this.loading = true;
+    
+    this.coursesService.findLessons(this.course.id, "asc", this.lastPageLoaded)
+    .pipe(
+      finalize(() => this.loading = false)
+    )
+    .subscribe(
+      lessons => this.lessons = this.lessons.concat(lessons)
+    );
   }
 
 }
