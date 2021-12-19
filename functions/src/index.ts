1d4098f657f31b020331cd253478f22b2c4e2b31
Victor Escudero <victor.escudero@bestsecret.com>
2021-12-19 20:48:37 +0100

    Simpler way to update a counter

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,9 @@
 import * as functions from "firebase-functions";
 import { db } from "./init";
 
+import { firestore } from 'firebase-admin/lib/firestore';
+import FieldValue = firestore.FieldValue;
+
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 
@@ -17,17 +20,17 @@ export const onAddCourseUpdatePromoCounter =
             const course = snap.data();
 
             if(course.promo) {
-                return db.runTransaction(async transaction => {
-                    
-                    const counterRef = db.doc("courses/stats");
-
-                    const snap = await transaction.get(counterRef);
-
-                    const stats = snap.data() ?? { totalPromo: 0 };
-
-                    stats.totalPromo += 1;
-
-                    transaction.set(counterRef, stats);
+                // This way is simpler but fails if stats document doesn't exist
+                return db.doc("courses/stats").update({
+                    totalPromo: FieldValue.increment(1)
                 });
+                
+                // return db.runTransaction(async transaction => {
+                //     const counterRef = db.doc("courses/stats");
+                //     const snap = await transaction.get(counterRef);
+                //     const stats = snap.data() ?? { totalPromo: 0 };
+                //     stats.totalPromo += 1;
+                //     transaction.set(counterRef, stats);
+                // });
             }
         });
