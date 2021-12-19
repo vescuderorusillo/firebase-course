f7e28ef2191a15b3a659dbedf184048101d99949
Victor Escudero <victor.escudero@bestsecret.com>
2021-12-19 20:36:53 +0100

    Update a counter in Firestore in a transactional way with Cloud Functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,5 @@
 import * as functions from "firebase-functions";
+import { db } from "./init";
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -12,4 +13,21 @@ export const onAddCourseUpdatePromoCounter =
         .firestore.document("courses/{courseId}")
         .onCreate(async (snap, context) => {
             functions.logger.debug(`Running add course trigger for courseId ${context.params.courseId}`);
-        })
+
+            const course = snap.data();
+
+            if(course.promo) {
+                return db.runTransaction(async transaction => {
+                    
+                    const counterRef = db.doc("courses/stats");
+
+                    const snap = await transaction.get(counterRef);
+
+                    const stats = snap.data() ?? { totalPromo: 0 };
+
+                    stats.totalPromo += 1;
+
+                    transaction.set(counterRef, stats);
+                });
+            }
+        });
